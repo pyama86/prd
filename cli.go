346c7b4f6c6534784c129b09c51ed3a6f39c02da
pyama86 <pyama@pepabo.com>
2017-04-11 17:58:43 +0900

    support multifile

--- cli.go
+++ cli.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -24,11 +25,19 @@ type CLI struct {
 	outStream, errStream io.Writer
 }
 
+type Result struct {
+	Name string  `json:"name"`
+	Max  float64 `json:"max"`
+	Min  float64 `json:"min"`
+	Avg  float64 `json:"avg"`
+}
+
+type Results []Result
+
 // Run invokes the CLI with the given arguments.
 func (cli *CLI) Run(args []string) int {
 	var (
 		pastMin int
-		file    string
 
 		version bool
 	)
@@ -36,9 +45,6 @@ func (cli *CLI) Run(args []string) int {
 	// Define option flag parse
 	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
 	flags.SetOutput(cli.errStream)
-
-	flags.StringVar(&file, "file", "", "")
-	flags.StringVar(&file, "f", "", "(Short)")
 
 	flags.IntVar(&pastMin, "past_min", 86400, "")
 	flags.IntVar(&pastMin, "p", 86400, "(Short)")
@@ -55,40 +61,53 @@ func (cli *CLI) Run(args []string) int {
 		fmt.Fprintf(cli.errStream, "%s version %s\n", Name, Version)
 		return ExitCodeOK
 	}
+	results := Results{}
+	for _, file := range flags.Args() {
+		inf, err := rrd.Info(file)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	inf, err := rrd.Info(file)
-	if err != nil {
-		log.Fatal(err)
-	}
+		end := time.Unix(int64(inf["last_update"].(uint)), 0)
+		start := end.Add(time.Duration(pastMin*-1) * time.Minute)
 
-	end := time.Unix(int64(inf["last_update"].(uint)), 0)
-	start := end.Add(time.Duration(pastMin*-1) * time.Minute)
+		r, err := rrd.Fetch(file, "MAX", start, end, 1*time.Second)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	r, err := rrd.Fetch(file, "MAX", start, end, 1*time.Second)
-	if err != nil {
-		log.Fatal(err)
-	}
+		var max, min float64
+		var avgv float64
+		var avgc float64
+		for i := 0; i < len(r.DsNames); i++ {
+			for n := 0; n < r.RowCnt; n++ {
+				v := math.Floor(r.ValueAt(i, n))
 
-	var max, min float64
-	var avgv float64
-	var avgc int
-	for i := 0; i < len(r.DsNames); i++ {
-		for n := 0; n < r.RowCnt; n++ {
-			v := math.Floor(r.ValueAt(i, n))
+				if v > 0 {
+					avgv += v
+					if max < v {
+						max = v
+					}
 
-			if v > 0 {
-				avgv += v
-				if max < v {
-					max = v
+					if min == 0 || v < min {
+						min = v
+					}
+					avgc++
 				}
-
-				if min == 0 || v < min {
-					min = v
-				}
-				avgc++
 			}
 		}
+		results = append(results, Result{
+			Name: r.Filename,
+			Max:  max,
+			Min:  min,
+			Avg:  math.Floor(avgv / avgc),
+		})
+
 	}
-	fmt.Printf(`{ "max": %f, "min": %f, "avg": %f }`, max, min, math.Floor(avgv/float64(avgc)))
+	j, err := json.Marshal(results)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(j))
 	return ExitCodeOK
 }
